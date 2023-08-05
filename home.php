98c53fb102b2b52dddd40bb0f1606e7969620a66
Yout-bit <stormcreeper18@gmail.com>
2023-08-05 19:28:22 +0100

    first commit

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php 
+$title = "Home";
+include 'inc/header.php';
+?>
+<div class="body">
+  <div class="tab-content" id="logincontent">
+    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Home</div>
+    <div class="tab-pane fade" id="play" role="tabpanel" aria-labelledby="profile-tab">Play</div>
+    <div class="tab-pane fade" id="leaderboard" role="tabpanel" aria-labelledby="profile-tab">Leaderboard</div>
+    <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="profile-tab">Register</div>
+</div>  
+
+<?php
+include "footer.php";  
+?>
---
+++ inc/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en" class="h-100" data-bs-theme="auto">
+  <head>
+    <title>ECM1417 Web Dev | Coursework</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <link 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" 
+    crossorigin="anonymous"
+    >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Che Barber">
+    <meta name="generator" content="">
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+
+  </head>
+  <body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+      <div class="container">
+          <a class="navbar-brand" href="#home">Home</a>
+          <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+              <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ms-auto">
+                  <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#play">Play</a></li>
+                  <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#leaderboard">Leaderboard</a></li>
+                  <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#register">Register</a></li>
+              </ul>
+          </div>
+      </div>
+    </nav>
